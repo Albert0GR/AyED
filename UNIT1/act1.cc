43784ab54f40372957cf491405b65d16b3eb714c
Albert0GR <albertogr0121@gmail.com>
2022-10-25 06:51:48 -0500

    Initial commit

---
+++ UNIT1/act1.cc
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+#define length 4
+
+int arr[length];
+int pot[length];
+
+void leerDatos();
+void potenciaArreglo(int tam, int *arr, int *pot);
+void mostrar(int tam, int *n);
+
+int main(){
+    
+    leerDatos();
+    potenciaArreglo(4,arr,pot);
+    printf("Resultado potencias\n");
+    mostrar(4,arr);
+    
+}
+void leerDatos(){
+    for (int i = 0; i < length; i++)
+    {
+        printf("ingresa el numero [%d]",i);
+        scanf("%d",&arr[i]);
+
+ 
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("ingresa la potencia para el numero [%d]",i);
+        scanf("%d",&pot[i]);
+    }
+    
+    
+
+}
+
+void potenciaArreglo(int tam, int *arr, int *pot){
+    printf("Numeros:\n");
+    mostrar(tam,arr);
+    for(int j=0; j<tam; j++){
+         *(arr+j) =pow(*(arr+j),*(pot+j));
+    }
+       
+
+}
+void mostrar(int tam, int *n){
+    {
+    for(int j=0; j<tam; j++)
+        printf("num[%d] = %d \n", j, *(n+j));
+        //printf("%d ",*(n+j));
+    }
+}
