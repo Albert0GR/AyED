9c017f476439dbe818245c8e20494857b9d4eb27
Albert0GR <albertogr0121@gmail.com>
2022-11-10 12:29:39 -0600

    Update funciones.h

--- Lista_ejemplo/listaheaders/funciones.h
+++ Lista_ejemplo/listaheaders/funciones.h
@@ -4,12 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-typedef struct snodo{ //snodo es el nombre de la estructura
+typedef struct snodo{       //snodo es el nombre de la estructura
     int valor;
-    struct snodo *sig; //El puntero siguiente para recorrer la lista enlazada
-}tnodo; //tnodo es el tipo de dato para declarar la estructura
+    struct snodo *sig;      //El puntero siguiente para recorrer la lista enlazada
+}tnodo;                     //tnodo es el tipo de dato para declarar la estructura
  
-typedef tnodo *tpuntero; //Puntero al tipo de dato tnodo para no utilizar punteros de punteros
+typedef tnodo *tpuntero;    //Puntero al tipo de dato tnodo para no utilizar punteros de punteros
  
 void insertarEnListaCabeza (tpuntero *cabeza, int e);
 void imprimirLista (tpuntero cabeza);
