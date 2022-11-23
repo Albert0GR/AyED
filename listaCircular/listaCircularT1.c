06587c82b277c25f31e3e664e12fa2110f1a750e
Albert0GR <albertogr0121@gmail.com>
2022-11-23 12:50:23 -0600

    listaCircular modificacionfinal

--- listaCircular/listaCircularT1.c
+++ listaCircular/listaCircularT1.c
@@ -14,7 +14,7 @@ int element(int i);
 //inserta el valor "x" al final de la lista y devuelve 1 si esto se se realiza con éxito, o 0 si no es posible
 int insert(int x);
 //elimina el el valor contenido en la i-ésima posición, o VACIO, si la lista no contiene elemento en esa posición.
-//int remov(listaCircular *lista, int i);
+int remov(int i);
 //inicializar la lista
 
 void inicializa();
@@ -34,10 +34,15 @@ int main(){
     insert(77);
     insert(66);
     insert(55);
-    insert(44);
-    insert(33);
-    insert(22);
+    //insert(44);
+    //insert(33);
+    //insert(22);
     imprime();
+    element(3);
+
+    remov(5);
+    imprime();
+    remov(1);
 
 
 
@@ -51,9 +56,7 @@ int full(){
     return 0;
 }
 
-int element(int i){
-  
-}
+
 int insert(int x){
     if(!full()){
         lista[fin] = x;
@@ -88,4 +91,33 @@ void imprime(){
 
 int element(int i){
     
+        if (i >= 0 && i <= size)
+    {
+        printf("\nel valor de la lista en el indice [%d] es: %d",i,lista[i]);
+        return lista[i];
+
+    }else{
+        printf("\nindice no valido");
+        return 0;
+    }
+}
+
+int remov(int i){
+    
+        if (i >= 0 && i <= size)
+    {
+        if(lista[i]!=0){
+             printf("\n se borrara el valor de la lista en el indice [%d] que es: %d",i,lista[i]);
+             lista[i]=0;
+            return 1;
+        }else{
+            printf("\nla lista en este idice ya estaba vacia");
+            return 0;
+        }
+       
+
+    }else{
+        printf("\nindice no valido");
+        return 0;
+    }
 }
